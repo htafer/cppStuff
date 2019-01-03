fc03611e6e435457ba5f3c3d46ef4b9ad651f915
Hakim Tafer <htafer@gmail.com>
2019-01-03 14:52:27 +0100

    encapsulating SDL code into screen class

---
+++ Particle_Test/cmake-build-debug/CMakeFiles/Particle_Test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Particle_Test.dir/main.cpp.o"
+  "Particle_Test.pdb"
+  "Particle_Test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Particle_Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
