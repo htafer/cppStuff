f0630577faba36b72ced7bdc7969d442732680c6
Hakim Tafer <htafer@gmail.com>
2019-01-04 21:56:21 +0100

    custom exception class

--- BasicException/main.cpp
+++ BasicException/main.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
 
 void mightGoWrong();
-
+void usesMightGoWrong();
 using namespace std;
 int main () {
-
     try {
+        //We catch the error
         mightGoWrong();
     }
     catch(int e){
-        cout << "Error Code " << e << endl;
+        cout << "Error Message: " << e << endl;
+    }
+    catch(char const *e){
+        cout <<  "Error Message: " << e << endl;
+    }
+    catch(string &e){
+        cout <<  "Error Message: " << e << endl;
     }
     std::cout << "Still running" << std::endl;
     return 0;
 }
 
+void usesMightGoWrong(){
+    //Exception get thrown all the way up
+    mightGoWrong();
+}
+
 void mightGoWrong(){
-    bool error = true;
-    if(error){
-        throw 8;
+    bool error1 = false;
+    bool error2 = true;
+    if(error1){
+        throw "Error1";
+    }
+    else if(error2){
+        throw string("Something went wrong");
     }
 }
