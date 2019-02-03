ddd7d450bf189ead96dba3305bd0e0a3b99e21d2
Hakim Tafer <htafer@gmail.com>
2019-02-03 21:48:56 +0100

    autoRangeBasedLoops

---
+++ rangeBasedLoops/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+    auto texts = {"one","two","three"};
+    for(auto text: texts){
+        cout << text << endl;
+    }
+    auto numbers = {1,2,3,4};
+    for(auto number: numbers){
+        cout<<number<<endl;
+    }
+
+        std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
